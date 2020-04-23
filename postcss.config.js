95a5ab62b02ea9eb42918555f0380d3b6aa33ea8
zaoyu63 <zaoyu63@163.com>
2020-04-23 10:31:18 +0800

    rem布局，样式自动补全

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,16 @@
 module.exports = {
   plugins: {
-    autoprefixer: {}
+    autoprefixer: {
+      browsers: ['Android >= 4.0', 'iOS >= 7']
+    },
+    'postcss-pxtorem': {
+      rootValue: 37.5, //默认根目录字体大小(px)
+      unitPrecision: 5, //保留小数位
+      propList: ['*'],
+// selectorBlackList: [''], //过滤的类名
+      replace: true, //默认直接替换属性
+      mediaQuery: false,
+      minPixelValue: 6, //所有小于设置的样式都不被转换
+    }
   }
 };
